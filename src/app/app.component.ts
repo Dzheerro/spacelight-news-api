eb8e97adcbfc49b05dcdb6decacdf6f4be89a695
cloudlet <mark.bularin2005@gmail.com>
2024-06-06 01:26:07 +0300

    implemented an article component for a single article + added library to format date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 import { ActionService } from './__services/action.service';
+
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button'
+import { DatePipe } from './__helpers/date.pipe';
+import { TrimPipe } from './__helpers/trim.pipe';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, CardModule, ButtonModule, DatePipe, TrimPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   articles: any;
- 
+
   constructor(private actionService: ActionService) { }
 
   ngOnInit(): void {
@@ -20,9 +28,8 @@ export class AppComponent implements OnInit{
   }
 
   getArticles() {
-    this.actionService.getArticles().subscribe( (response: any) => {
-      console.log(response);
-      
+    this.actionService.getArticles().subscribe((response: any) => {
+      this.articles = response.results;
     })
   }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'articles', component: AppComponent },
+    { path: 'article/:id', component: ArticleComponent},
+
+    // Default Routes
+    { path: '', redirectTo: '/articles', pathMatch: 'full' },
+    { path: '**', redirectTo: '/articles'}
+];
