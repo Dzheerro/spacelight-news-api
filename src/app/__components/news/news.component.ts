ccfe009330ca2189a1895242316a0e57d10a370c
cloudlet <mark.bularin2005@gmail.com>
2024-06-08 12:29:17 +0300

    added groups to filter keywords

--- src/app/__components/news/news.component.ts
+++ src/app/__components/news/news.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
@@ -18,11 +18,14 @@ import { ActionService } from '../../__services/action.service';
   imports: [CommonModule, CardModule, ButtonModule, DatePipe, TrimPipe, RouterLink, AutoCompleteModule, FormsModule],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss'
+
 })
+
 export class NewsComponent implements OnInit {
   articles: any[] = [];
-  filteredArticles: any[] = [];
+  filteredGroups: any[] = [];
   searchQuery: string = '';
+  totalResults: number = 0;
 
   constructor(private actionService: ActionService, private router: Router) { }
 
@@ -33,26 +36,36 @@ export class NewsComponent implements OnInit {
   getArticles() {
     this.actionService.getArticles().subscribe((response: any) => {
       this.articles = response.results;
-      this.filteredArticles = response.results;
     });
   }
 
   filterArticles(event: any) {
-    const query = event.query.toLowerCase();
-    this.filteredArticles = this.articles.filter(article => 
-      article.title.toLowerCase().includes(query) || 
-      article.summary.toLowerCase().includes(query)
-    );
+    const query = event.query;
+
+    let titleMatches = this.articles.filter(article => article.title.toLowerCase().includes(query));
+    let summaryMatches = this.articles.filter(article => article.summary.toLowerCase().includes(query) && !article.title.toLowerCase().includes(query));
+
+    this.filteredGroups = [
+      {
+        label: 'Titles',
+        items: titleMatches
+      },
+      {
+        label: 'Summary',
+        items: summaryMatches
+      }
+    ];
+
+    this.totalResults = titleMatches.length + summaryMatches.length;
   }
 
   highlight(text: string): string {
     if (!this.searchQuery) return text;
-    const query = this.searchQuery.toLowerCase();
+    const query = this.searchQuery;
     return text.replace(new RegExp(query, 'gi'), match => `<span class="highlight">${match}</span>`);
   }
 
   redirect(id: number) {
     this.router.navigate([`/article/${id}`]);
   }
-  
 }
